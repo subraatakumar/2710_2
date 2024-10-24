ab210fd1c82d2adea949adf13d7684e8b3cdf916
subraatakumar <business.ttrc@gmail.com>
2024-10-24 20:27:14 +0530

    redux custom middleware

---
+++ src/redux/custom_middleware/redux_logger.js
@@ -0,0 +1,22 @@
+// Without middleware: dispatch -> action -> reducer -> update state
+// With middleware: dispatch -> action -> middleware -> reducer -> update state
+
+const loggerMiddleware = (store) => (next) => (action) => {
+  console.log("Previous state: ", store.getState());
+  console.log("Action:", action);
+
+  let modifiedAction;
+  // Call the next middleware or reducer
+  if (action.payload > 10) {
+    modifiedAction = { ...action, payload: 10 };
+  } else {
+    modifiedAction = { ...action };
+  }
+  console.log("Modified action:", modifiedAction);
+  const result = next(modifiedAction);
+
+  console.log("Updated State:", store.getState());
+
+  return result;
+};
+export default loggerMiddleware;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
 import counterReducer from "./reducers/counterReducer";
 import authReducer from "./reducers/authReducer";
+import loggerMiddleware from "./custom_middleware/redux_logger";
 
 const combineReducer = combineReducers({ counterReducer, authReducer });
 
-export const store = createStore(combineReducer);
+export const store = createStore(
+  combineReducer,
+  applyMiddleware(loggerMiddleware)
+);
