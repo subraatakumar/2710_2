e5052ddbef4014a5027dfe07e90d254c6f51f231
subraatakumar <business.ttrc@gmail.com>
2024-10-24 20:29:40 +0530

    condition commented

--- src/redux/custom_middleware/redux_logger.js
+++ src/redux/custom_middleware/redux_logger.js
@@ -5,15 +5,16 @@ const loggerMiddleware = (store) => (next) => (action) => {
   console.log("Previous state: ", store.getState());
   console.log("Action:", action);
 
-  let modifiedAction;
+  //   let modifiedAction;
   // Call the next middleware or reducer
-  if (action.payload > 10) {
-    modifiedAction = { ...action, payload: 10 };
-  } else {
-    modifiedAction = { ...action };
-  }
-  console.log("Modified action:", modifiedAction);
-  const result = next(modifiedAction);
+  //   if (action.payload > 10) {
+  //     modifiedAction = { ...action, payload: 10 };
+  //   } else {
+  //     modifiedAction = { ...action };
+  //   }
+  //   console.log("Modified action:", modifiedAction);
+  //   const result = next(modifiedAction);
+  const result = next(action);
 
   console.log("Updated State:", store.getState());
 
